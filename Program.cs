6d4d5d8c393ea96abc9eaf0e445b964bdfc9a53b
=jahanzaib-div <jahanzaibmaqbool0307@gmail.com>
2023-12-09 20:52:04 +0500

    input & output

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("hello world");
+Console.WriteLine("C# (C-Sharp) is a programming language developed by Microsoft that runs on the .NET Framework.");
+Console.WriteLine("C# is used to develop web apps, desktop apps, mobile apps, games and much more.");
+
+// input from user
+
+string name = Console.ReadLine();
+Console.WriteLine("Your Name is " + name); // string concat method
+
+//2nd method using placeholder
+Console.WriteLine("Your name is {0}", name);
